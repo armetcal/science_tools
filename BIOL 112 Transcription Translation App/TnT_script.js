266c6d5b75c0a5e694fcd64a4f946ce80763121e
armetcal <armetcal@edu.uwaterloo.ca>
2021-11-27 01:22:46 -0800

    Main functionalities done

--- BIOL 112 Transcription Translation App/TnT_script.js
+++ BIOL 112 Transcription Translation App/TnT_script.js
@@ -1,4 +1,17 @@
 // Assign default button values
+//------Global----------
+var template_strand = '';
+var num_start = '';
+var mrna_sequence = '';
+var pep_out = '';
+var button1_value = 0;
+var button2_value = 0;
+var button3_value = 0;
+var button4_value = 0;
+var last_answer = 0;
+var last_hint = 0;
+const total_length = 35;
+var clicked = 0;
 
 function do_onclick() {
 
@@ -11,13 +24,13 @@ function do_onclick() {
     document.getElementById("num_codons").innerHTML = description1 + num_codons.toString() + description2;
 
     // Variables
-    var button1_value = 0;
-    var button2_value = 0;
-    var button3_value = 0;
-    var button4_value = 0;
-    var last_answer = 0;
-    var last_hint = 0;
-    const total_length = 35;
+    button1_value = 0;
+    button2_value = 0;
+    button3_value = 0;
+    button4_value = 0;
+    clicked = 0;
+    last_answer = 0;
+    last_hint = 0;
 
     // Codon Table
     const ct = {'Ala': ['GCA','GCC','GCG','GCT'], 
@@ -46,9 +59,6 @@ function do_onclick() {
     var ct_stop = ct_stop.Stp;
     var ct_nostop = $.extend( true, {}, ct);
     delete ct_nostop.Stp;
-    //console.log(ct);
-    //console.log(ct_stop);
-    //console.log(ct_nostop);
     // Assign top/bottom strand
     var temp = ['top','bottom'];
     var random2 = Math.round(Math.random());
@@ -80,8 +90,6 @@ function do_onclick() {
         post.push(bases[Math.floor(Math.random() * 4)]);
     }
     var complete_string = pre.join('') + final_string + post.join('');
-    console.log(final_string);
-    console.log(complete_string);
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -95,7 +103,6 @@ function do_onclick() {
     var fake_cat = Math.round(Math.random()+1);
     // Add fake atg
     var changeable = Array.from({length: (total_length-3)}, (_, index) => index + 1);
-    console.log(changeable);
     var changeable = changeable.filter(b => !(untouchable.includes(b)));
     if(fake_atg == 1){
         var temp = changeable[Math.floor(Math.random() * changeable.length)];
@@ -128,8 +135,8 @@ function do_onclick() {
     }
     var num_atg = num_atg.filter(b => b != extra_bases_1);
     for(var i = 0; i<num_atg.length; i++){
-        var temp = i+3+(3*num_codons);
-        var temp2 = complete_string.substring(temp,temp+2);
+        var temp = num_atg[i]+3+(3*num_codons);
+        var temp2 = complete_string.substring(temp,temp+3);
         if(ct_stop.includes(temp2)){
             if(changeable.includes(temp)){
                 var complete_string = complete_string.split('')
@@ -160,9 +167,9 @@ function do_onclick() {
         indexOccurence = complete_string.indexOf('CAT', indexOccurence + 1);
     }
     for(var i = 0; i<num_cat.length; i++){
-        var temp = i+3+(3*num_codons);
-        var temp2 = complete_string.substring(temp,temp+2);
-        var temp3 = ['TTA','CTA','TCA']
+        var temp = num_cat[i]-3-(3*num_codons);
+        var temp2 = complete_string.substring(temp,temp+3);
+        var temp3 = ['TTA','CTA','TCA'];
         if(temp3.includes(temp2)){
             if(changeable.includes(temp)){
                 var complete_string = complete_string.split('')
@@ -190,12 +197,12 @@ function do_onclick() {
     
     // Reverse if necessary
     if(direction=='bottom'){
-        var template_strand = 'Top';
+        template_strand = 'Top';
         var complete_string = complete_string.split('');
         var complete_string = complete_string.reverse();
         var complete_string = complete_string.join('');
     } else {
-        var template_strand = 'Bottom'
+        template_strand = 'Bottom'
     }
     // Create template strand
     const base_key = { // coding: [template, mRNA]
@@ -204,7 +211,6 @@ function do_onclick() {
         'G':['C','G'],
         'C':['G','C']
     }
-    var df = complete_string.split('');
     var coding = complete_string;
     var template = ''
     var mRNA = ''
@@ -213,9 +219,6 @@ function do_onclick() {
         var template = template + base_key[temp][0];
         var mRNA = mRNA + base_key[temp][1];
     }
-    console.log(coding);
-    console.log(template);
-    console.log(mRNA);
         
     // Combine - will have to print one on top of each other
     if(direction=='top'){
@@ -227,4 +230,48 @@ function do_onclick() {
     }
     document.getElementById("output_top").innerHTML = output_top;
     document.getElementById("output_bottom").innerHTML = output_bottom;
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+    // Correct answers:
+    // Direction defined above
+    // # Start codons
+    var temp = num_atg.length + num_cat.length
+    num_start = temp.toString()
+    // mRNA sequence
+    if(direction=='bottom'){
+        var temp = complete_string.split('');
+        var temp = temp.reverse();
+        var temp = temp.join('');
+        mrna_sequence = '5-' + temp + '-3';
+    } else {
+        mrna_sequence = '5-' + complete_string + '-3';
+    }
+    // Peptide sequence
+    var pep = ['Met']
+    function getKeyByValue(object, value) {
+        return Object.keys(object).find(key => object[key].includes(value));
+    }
+    for(var i = 0; i<protein.length; i++){
+        var temp = protein[i];
+        var temp2 = getKeyByValue(ct,temp);
+        pep.push(temp2);
+    }
+    pep_out = 'N-' + pep.join('-') + '-C';
+}
+
+function button_show_onclick(){
+    if(clicked == 0 ) {
+      document.getElementById("answer1").innerHTML = template_strand;
+      document.getElementById("answer2").innerHTML = num_start;
+      document.getElementById("answer3").innerHTML = mrna_sequence;
+      document.getElementById("answer4").innerHTML = pep_out;
+      clicked = 1;
+    } else {
+      document.getElementById("answer1").innerHTML = "";
+      document.getElementById("answer2").innerHTML = "";
+      document.getElementById("answer3").innerHTML = "";
+      document.getElementById("answer4").innerHTML = "";
+      clicked = 0;
+    }
 }
